168b4578d136df0de60a203596e5ad099732e7a9
vjern <jocelyn.vernay@gmail.com>
2024-04-24 17:50:50 +0200

    Forbid blank urls

--- src/schema.py
+++ src/schema.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class Entry(BaseModel):
     url: str
+
+    @field_validator("url")
+    @classmethod
+    def validate_url(cls, url: str):
+        if not url.strip():
+            # Or use a off-the-shelf parser or a regex for more rigorous validation
+            raise ValueError("Not an url")
+        return url
